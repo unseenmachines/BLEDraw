8e3a6736cf6689e735fe7fabbdf72c4e9b8b2d6e
Elliot Sinyor <git@elliotsinyor.com>
2016-02-27 15:37:58 -0500

    Extend NSData to include a convenience function to get the first byte

--- BLEDraw/BLEMessage.swift
+++ BLEDraw/BLEMessage.swift
@@ -28,3 +28,22 @@ func messageTypeFromByte(byte: UInt8) -> MessageType {
     }
     
 }
+
+extension NSData {
+   
+    func firstByte() -> UInt8! {
+        
+        guard self.length > 0 else { return nil }
+        
+        var byte : UInt8 = 0x00
+       
+        let firstByte = self.subdataWithRange(NSRange(location: 0, length: 1))
+       
+        firstByte.getBytes(&byte, length: 1)
+        
+        return byte
+    }
+    
+}
+
+
