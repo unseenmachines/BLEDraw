ebb9b6ba7eb2c8862f1a65ef8e9d7f3038355f38
Elliot Sinyor <git@elliotsinyor.com>
2016-02-27 15:36:53 -0500

    Add new Message file

---
+++ BLEDraw/BLEMessage.swift
@@ -0,0 +1,10 @@
+//
+//  BLEMessage.swift
+//  BLEDraw
+//
+//  Created by Elliot Sinyor on 2016-02-27.
+//  Copyright © 2016 elliotsinyor. All rights reserved.
+//
+
+import Foundation
+import UIKit
