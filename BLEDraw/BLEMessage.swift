e419e3b63e6de73592fbbac1a4995bb65fe82693
Elliot Sinyor <git@elliotsinyor.com>
2016-02-27 15:37:31 -0500

    Define message type enum and function to determine type from byte

--- BLEDraw/BLEMessage.swift
+++ BLEDraw/BLEMessage.swift
@@ -8,3 +8,23 @@
 
 import Foundation
 import UIKit
+
+enum MessageType : UInt8 {
+    case unknown = 0x00
+    case start = 0x01
+    case point = 0x02
+}
+
+func messageTypeFromByte(byte: UInt8) -> MessageType {
+   
+    switch byte {
+        
+    case 0x01:
+        return MessageType.start
+    case 0x02:
+        return MessageType.point
+    default:
+        return MessageType.unknown
+    }
+    
+}
