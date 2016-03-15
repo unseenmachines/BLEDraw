57f2ae937b1a2ddc92defa055fea0600ecb46340
Elliot Sinyor <git@elliotsinyor.com>
2016-03-14 20:26:50 -0400

    Move helper to end of file

--- BLEDraw/DrawView.swift
+++ BLEDraw/DrawView.swift
@@ -9,21 +9,7 @@
 import Foundation
 import UIKit
 
-extension UIColor {
-    
-    func rgb() -> (red:CGFloat, green:CGFloat, blue:CGFloat, alpha:CGFloat) {
-        var fRed : CGFloat = 0
-        var fGreen : CGFloat = 0
-        var fBlue : CGFloat = 0
-        var fAlpha: CGFloat = 0
-        if self.getRed(&fRed, green: &fGreen, blue: &fBlue, alpha: &fAlpha) {
-            return (red:fRed, green:fGreen, blue:fBlue, alpha:fAlpha)
-        } else {
-            // Could not extract RGBA components:
-            return (red: 0, green: 0, blue: 0, alpha: 0)
-        }
-    }
-}
+
 
 class DrawView : UIView {
  
@@ -118,4 +104,24 @@ class DrawView : UIView {
     }
   
     
+    
 }
+
+//# MARK: Helpers
+
+extension UIColor {
+    
+    func rgb() -> (red:CGFloat, green:CGFloat, blue:CGFloat, alpha:CGFloat) {
+        var fRed : CGFloat = 0
+        var fGreen : CGFloat = 0
+        var fBlue : CGFloat = 0
+        var fAlpha: CGFloat = 0
+        if self.getRed(&fRed, green: &fGreen, blue: &fBlue, alpha: &fAlpha) {
+            return (red:fRed, green:fGreen, blue:fBlue, alpha:fAlpha)
+        } else {
+            // Could not extract RGBA components:
+            return (red: 0, green: 0, blue: 0, alpha: 0)
+        }
+    }
+}
+
